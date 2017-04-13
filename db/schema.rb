2cd223cb4fbe66907a8c5dd15f7d73b985d62546
Lydia Greco <lydiagreco@Lydias-MacBook-Air.local>
2017-04-13 16:43:40 -0600

    Added portfolio items via the resource generator fixed version

--- db/schema.rb
+++ db/schema.rb
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20170406032719) do
 
   create_table "portfolios", force: :cascade do |t|
     t.string   "title"
-    t.string   "string"
+    
     t.string   "subtitle"
     t.text     "body"
     t.text     "main_image"
