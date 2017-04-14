6f6c4212f44eafcc54f2bb969427dbf84ef60711
Lydia Greco <lydiagreco@Lydias-MacBook-Air.local>
2017-04-13 23:57:44 -0600

    trying to fix bug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170406032719) do
+ActiveRecord::Schema.define(version: 20170406034050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,6 @@ ActiveRecord::Schema.define(version: 20170406032719) do
 
   create_table "portfolios", force: :cascade do |t|
     t.string   "title"
-    
     t.string   "subtitle"
     t.text     "body"
     t.text     "main_image"
