a5cf1324e7afde6d177575540f08318a2d8f72e4
Lydia Greco <lydiagreco@Lydias-MacBook-Air.local>
2017-09-26 13:54:28 -0600

    Implemented basic authentication and fixed migration file issues

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -10,4 +10,5 @@ class Skill < ApplicationRecord
   def set_defaults
     self.badge ||= Placeholder.image_generator(height: '250', width: '250')
   end
+end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+
+  devise_for :users
   resources :portfolios, except: [:show]
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-=======
+
   resources :portfolios
   get 'pages/home'
->>>>>>> resource-generator
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
