000968f9fbe41b3eec4b119d6215c03e61ca2979
Lydia Greco <lydiagreco@Lydias-MacBook-Air.local>
2017-11-27 16:41:51 -0700

    Updated portfolio to include attr for postition

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,16 +1,17 @@
 class Portfolio < ApplicationRecord
-
-  validates_presence_of :title, :body, :main_image, :thumb_image
-
   has_many :technologies
   accepts_nested_attributes_for :technologies,
-                                reject_if: lambda {|attrs| attrs['name'].blank?}
+                                reject_if: lambda { |attrs| attrs['name'].blank? }
 
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   def self.angular
     where(subtitle: 'Angular')
+  end
+
+  def self.by_position
+    order("position ASC")
   end
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
