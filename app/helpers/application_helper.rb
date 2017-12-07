a99c92922d48994c8d62c3b6c197344a3cfa54a0
Lydia Greco <lydiagreco@Lydias-MacBook-Air.local>
2017-12-06 17:29:07 -0700

    Refactored alerts into help method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,7 +17,7 @@ module ApplicationHelper
   end
 
   def copyright_generator
-    GrecoViewTool::Renderer.copyright 'Lydia Greco', 'All rights reserved'
+    DevcampViewTool::Renderer.copyright 'Jordan Hudgens', 'All rights reserved'
   end
 
   def nav_items
@@ -57,6 +57,18 @@ module ApplicationHelper
 
   def active? path
     "active" if current_page? path
+  end
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Jordan Hudgens Portfolio", sticky: false)
   end
 
 end
