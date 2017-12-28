4fbe69a3e0ab5a166cd98fa3aa36f10cbe0a31cc
Lydia Greco <lydiagreco@Lydias-MacBook-Air.local>
2017-12-27 19:58:02 -0700

    Implemented custom scope for recent blogs and hide draft post from non admins

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,13 +3,13 @@ module ApplicationCable
     identified_by :current_user
 
     def guest_user
-     guest = GuestUser.new
-     guest.id = guest.object_id
-     guest.name = "Guest User"
-     guest.first_name = "Guest"
-     guest.last_name = "User"
-     guest.email = "guest@user.com"
-     guest
+      guest = GuestUser.new
+      guest.id = guest.object_id
+      guest.name = "Guest User"
+      guest.first_name = "Guest"
+      guest.last_name = "User"
+      guest.email = "guest@user.com"
+      guest
     end
 
     def connect
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,7 +2,7 @@ class Blog < ApplicationRecord
   enum status: { draft: 0, published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
-  
+
   validates_presence_of :title, :body
 
   belongs_to :topic
@@ -16,5 +16,8 @@ class Blog < ApplicationRecord
   def self.featured_blogs
     limit(2)
   end
-end
 
+  def self.recent
+    order("created_at DESC")
+  end
+end
